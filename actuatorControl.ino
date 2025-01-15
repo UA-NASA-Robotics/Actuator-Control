04302d7db6d04879b9a4e068f55fd6853b0e861e
Jacob <jal260@uakron.edu>
2025-01-14 19:19:50 -0500

    Forgot to upload this with general update

--- actuatorControl.ino
+++ actuatorControl.ino
@@ -8,12 +8,14 @@ const int RPWM = 6; // GREEN
 const int buttonUpperPin = 10; // Orange
 const int buttonLowerPin = 11; // Orange
 
+int desiredSpeed = 0;  // Default value if no input is received
+int desiredPosition = 0; // Default value if no input is received
+
 struct ButtonStates {
   int buttonUpperState;
   int buttonLowerState;
 };
 
-// Takes the ButtonStates initialized booleans and updates them based on pin input
 ButtonStates getButtonStates() {
   ButtonStates states;
   states.buttonUpperState = digitalRead(buttonUpperPin);
@@ -22,27 +24,40 @@ ButtonStates getButtonStates() {
 }
 
 void speedDriveMotor(int percentage) {
-  // This ensures that the input into the function will alwasy be a valid percentage
+  // Ensures that the input into the function is always a valid percentage
   if (percentage > 100) percentage = 100;
   if (percentage < -100) percentage = -100;
 
-  // Converts the percentage into a pwm value, has to be absolute value because PWM cannot be negative
-  int pwmValue = map(abs(percentage),0,100,0,255);
+  // Converts the percentage into a PWM value (absolute value because PWM cannot be negative)
+  int pwmValue = map(abs(percentage), 0, 100, 0, 255);
 
-  // If percentage is positive, drive forward at the converted pwm percentage
+  // If percentage is positive, drive forward at the converted PWM percentage
   if (percentage > 0) {
-    analogWrite(LPWM,0);
-    analogWrite(RPWM,pwmValue);
+    analogWrite(LPWM, 0);
+    analogWrite(RPWM, pwmValue);
   }
-  // If perentage is negative, drive backward at the converted pwm percentage
+  // If percentage is negative, drive backward at the converted PWM percentage
   else if (percentage < 0) {
-    analogWrite(RPWM,0);
-    analogWrite(LPWM,pwmValue);
+    analogWrite(RPWM, 0);
+    analogWrite(LPWM, pwmValue);
   }
   // If percentage is zero, do nothing
   else {
     analogWrite(RPWM, 0);
     analogWrite(LPWM, 0);
+  }
+}
+
+// Function to parse serial input "<integer, integer>" format
+void parseSerialInput(String input) {
+  int commaIndex = input.indexOf(',');
+  if (commaIndex > 0) {
+    String speedStr = input.substring(1, commaIndex);
+    String positionStr = input.substring(commaIndex + 1, input.length() - 1); // Remove the '>'
+    
+    // Convert strings to integers
+    if (speedStr != "None") desiredSpeed = speedStr.toInt();
+    if (positionStr != "None") desiredPosition = positionStr.toInt();
   }
 }
 
@@ -53,7 +68,7 @@ void setup() {
   pinMode(R_EN, OUTPUT);
   pinMode(L_EN, OUTPUT);
 
-  // For now these two are wired to be enable, we could just jump them with the 5v power to hold them constant
+  // For now these two are wired to be enabled, we could just jump them with the 5V power to hold them constant
   digitalWrite(R_EN, HIGH);
   digitalWrite(L_EN, HIGH);
 
@@ -64,11 +79,10 @@ void setup() {
   Serial.begin(9600);
 }
 
+
 void loop() {
-  ButtonStates buttonStates = getButtonStates();
-
-  int desiredSpeed = -100; // Change to be desired speed as you test
-
+ButtonStates buttonStates = getButtonStates();
+  // Handle limit button conditions to modify desired speed
   if (desiredSpeed > 0 && buttonStates.buttonUpperState == 0) {
     desiredSpeed = 0;
   }
@@ -77,13 +91,20 @@ void loop() {
     desiredSpeed = 0;
   }
 
-  speedDriveMotor(desiredSpeed);
+    // Use the speedDriveMotor function to drive the motor
+    speedDriveMotor(desiredSpeed);
 
+  if (Serial.available() > 0) {
+
+    String input = Serial.readStringUntil('\n');
+    parseSerialInput(input);
+  }
+// Send feedback via serial
   Serial.print("<");
   Serial.print("5");
   Serial.print(",");
   Serial.print(buttonStates.buttonUpperState);
   Serial.print(",");
   Serial.print(buttonStates.buttonLowerState);
-  Serial.println(">");
+  Serial.println(">");    
 }
